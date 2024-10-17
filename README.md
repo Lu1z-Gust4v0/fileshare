a93182475321e30addbc9b53a869f219908fb8c3
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2024-10-17 19:55:41 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# File Share
+
+A website that you can upload and share files with your friends
+
+## Technologies
+
+- Nextjs
+- SpringBoot (Kotlin)
+- Postgresql
+- AWS S3
---
+++ backend/src/main/kotlin/com/fileshare/fileshare/DemoApplication.kt
@@ -0,0 +1,11 @@
+package com.fileshare.fileshare
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
